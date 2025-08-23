e5ffd689e913d187964c8e3b3bb56a07c1febfbd
ogheneokeno <ogheneokeno@gmail.com>
2025-08-24 00:37:19 +0100

    TaskTen

---
+++ print 1 to 20/TaskTen.java
@@ -0,0 +1,13 @@
+public class TaskTen{
+public static void main(String[]args){
+System.out.println("printing A to Z:");
+for(char i='A';i<='Z';i++){
+System.out.print( i + " ");
+}
+System.out.println("\n");
+for(char i='a';i<='z';i++){
+System.out.print( i + " ");
+}
+}
+}
+//product first 10 natural numbers;
