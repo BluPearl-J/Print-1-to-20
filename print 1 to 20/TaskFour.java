6f5e3db2bb5afc3cc45717c3497c2bf0dff4cdc8
ogheneokeno <ogheneokeno@gmail.com>
2025-08-23 17:28:55 +0100

    TaskFour

---
+++ print 1 to 20/TaskFour.java
@@ -0,0 +1,8 @@
+public class TaskFour{
+public static void main(String[]args){
+for(int i=0;i<=20;i++){
+int result=(i*i);
+System.out.println("The square of the number ,"+ i + "is:" + result);
+}
+}
+}
