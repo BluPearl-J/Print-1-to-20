12ac7a7b5f75cc621a05d16ffb54718d2b18732a
ogheneokeno <ogheneokeno@gmail.com>
2025-08-24 00:07:55 +0100

    TaskNine

---
+++ print 1 to 20/TaskNine.java
@@ -0,0 +1,13 @@
+public class TaskNine{
+public static void main(String[]args){
+System.out.println("CALCULATING THE PRODUCT OF FIRST TEN NATURAL NUMBERS:");
+int product=1;
+for(int i=1;i<=10;i++){
+product=product*i;
+
+System.out.printf("%d\n", i);
+}
+System.out.println("Total product of first 10 natural numbers is:" + product);
+}
+}
+//product first 10 natural numbers;
