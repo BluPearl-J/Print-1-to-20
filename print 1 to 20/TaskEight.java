d7977f583c392809aea008a894be5d3f687f725a
ogheneokeno <ogheneokeno@gmail.com>
2025-08-23 23:49:26 +0100

    TaskEight

---
+++ print 1 to 20/TaskEight.java
@@ -0,0 +1,12 @@
+public class TaskEight{
+public static void main(String[]args){
+System.out.println("The first 50 natural numbers are:");
+int total=0;
+for(int i=0;i<=50;i++){
+total+=i;
+System.out.printf("%S\n", i);
+}
+System.out.println("Total Sum first 50 natural numbers is:" + total);
+}
+}
+//Sum first 50 natural numbers;
