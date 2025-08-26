453f660c8676b457df26617abf411f62f00741b1
ogheneokeno <ogheneokeno@gmail.com>
2025-08-26 03:53:55 +0100

    TaskEleven multiplicationtable.java

---
+++ print 1 to 20/TaskElevenMultiplicationTable.java
@@ -0,0 +1,16 @@
+public class 	TaskElevenMultiplicationTable{
+public static void main(String[]args){
+TaskElevenMultiplicationTable taskelevenmultiplicationtable=new TaskElevenMultiplicationTable();
+int h=5;
+taskelevenmultiplicationtable.getMultiplication(h);
+}
+
+
+
+public void getMultiplication(int number){
+for(int i=1;i<=50;i++){
+int product=(number*i);
+System.out.println(number + "*" + i +" " + "equals:"+ " " + product);
+}
+}
+}
